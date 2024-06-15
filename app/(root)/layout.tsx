16bf563cd8e8ba1b099c06f9b5973e0c3220df35
Ragulsankar04 <ragulsankar2004@gmail.com>
2024-06-15 20:03:47 +0530

    Routes has been setup

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,5 @@
+import LeftSidebar from "@/components/LeftSidebar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -6,7 +8,7 @@ export default function RootLayout({
   return (
     <div>
         <main>
-            <p className="text-white-1">LEFT SIDE BAR</p>
+            <LeftSidebar />
             {children}
             <p className="text-white-1">RIGHT SIDE BAR</p>
         </main>
---
+++ components/LeftSidebar.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+import React from 'react'
+
+const LeftSidebar = () => {
+  return (
+    <section className="left_sidebar">
+      <nav className="flex flex-col gap-6">
+        <Link href="/">
+        </Link> 
+      </nav>
+    </section>
+  )
+}
+
+export default LeftSidebar
