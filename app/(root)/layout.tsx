6579907b849f5103410c5dd6f801718b9126c693
Ragulsankar04 <ragulsankar2004@gmail.com>
2024-06-25 11:32:48 +0530

    RightSideBar Updated

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,6 @@
 import LeftSidebar from "@/components/LeftSidebar";
+import RightSidebar from "@/components/RightSidebar";
+import Image from "next/image";
 
 export default function RootLayout({
   children,
@@ -6,11 +8,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div>
-        <main>
+    <div className="relative flex flex-col">
+        <main className="relative flex bg-black">
             <LeftSidebar />
-            {children}
-            <p className="text-white-1">RIGHT SIDE BAR</p>
+            <section className="border-2 border-red-500 flex flex-1 flex-col px-4 sm:px-14">
+              <div className="mx-auto flex w-full max-w-5xl flex-col">
+                <div>
+                  <Image />
+                </div>
+                <div>
+                  toaster(notification popups)
+                  {children}
+                </div>
+              </div>
+            </section>
+            <RightSidebar/>
         </main>
     </div>
   );
